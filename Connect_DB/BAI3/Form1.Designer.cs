42989c9a341dc59f0b07a05e04b1b3a490b3a596
ltaamlee <letam41225@gmail.com>
2025-03-01 23:22:45 +0700

    add manage app connect db

---
+++ Connect_DB/BAI3/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace BAI3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.dtp_dob = new System.Windows.Forms.DateTimePicker();
+            this.txt_faculty = new System.Windows.Forms.TextBox();
+            this.txt_gender = new System.Windows.Forms.TextBox();
+            this.btn_find = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.dataGridViewDiem = new System.Windows.Forms.DataGridView();
+            this.MaMonHoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenMonHoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diemthi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDiem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(129, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÌM KIẾM THÔNG TIN SINH VIÊN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập Mã SV: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ Tên SV: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ngày Sinh:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(340, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Mã khoa:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(37, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Giới tính:";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(134, 81);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(155, 22);
+            this.txt_id.TabIndex = 6;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(445, 78);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.ReadOnly = true;
+            this.txt_name.Size = new System.Drawing.Size(204, 22);
+            this.txt_name.TabIndex = 7;
+            this.txt_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dtp_dob
+            // 
+            this.dtp_dob.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtp_dob.Location = new System.Drawing.Point(134, 148);
+            this.dtp_dob.Name = "dtp_dob";
+            this.dtp_dob.Size = new System.Drawing.Size(106, 22);
+            this.dtp_dob.TabIndex = 8;
+            // 
+            // txt_faculty
+            // 
+            this.txt_faculty.Location = new System.Drawing.Point(445, 148);
+            this.txt_faculty.Name = "txt_faculty";
+            this.txt_faculty.ReadOnly = true;
+            this.txt_faculty.Size = new System.Drawing.Size(155, 22);
+            this.txt_faculty.TabIndex = 9;
+            this.txt_faculty.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_gender
+            // 
+            this.txt_gender.Location = new System.Drawing.Point(134, 219);
+            this.txt_gender.Name = "txt_gender";
+            this.txt_gender.ReadOnly = true;
+            this.txt_gender.Size = new System.Drawing.Size(78, 22);
+            this.txt_gender.TabIndex = 10;
+            this.txt_gender.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_find
+            // 
+            this.btn_find.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_find.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_find.Location = new System.Drawing.Point(430, 214);
+            this.btn_find.Name = "btn_find";
+            this.btn_find.Size = new System.Drawing.Size(99, 37);
+            this.btn_find.TabIndex = 11;
+            this.btn_find.Text = "Tìm";
+            this.btn_find.UseVisualStyleBackColor = false;
+            this.btn_find.Click += new System.EventHandler(this.btn_find_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(550, 214);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(99, 37);
+            this.btn_exit.TabIndex = 12;
+            this.btn_exit.Text = "Thoát";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // dataGridViewDiem
+            // 
+            this.dataGridViewDiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDiem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaMonHoc,
+            this.TenMonHoc,
+            this.diemthi});
+            this.dataGridViewDiem.Location = new System.Drawing.Point(41, 288);
+            this.dataGridViewDiem.Name = "dataGridViewDiem";
+            this.dataGridViewDiem.RowHeadersWidth = 51;
+            this.dataGridViewDiem.RowTemplate.Height = 24;
+            this.dataGridViewDiem.Size = new System.Drawing.Size(608, 143);
+            this.dataGridViewDiem.TabIndex = 13;
+            // 
+            // MaMonHoc
+            // 
+            this.MaMonHoc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaMonHoc.DataPropertyName = "MaMonHoc";
+            this.MaMonHoc.HeaderText = "Mã Môn Học";
+            this.MaMonHoc.MinimumWidth = 6;
+            this.MaMonHoc.Name = "MaMonHoc";
+            // 
+            // TenMonHoc
+            // 
+            this.TenMonHoc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenMonHoc.DataPropertyName = "TenMon";
+            this.TenMonHoc.HeaderText = "Tên Môn Học";
+            this.TenMonHoc.MinimumWidth = 6;
+            this.TenMonHoc.Name = "TenMonHoc";
+            // 
+            // diemthi
+            // 
+            this.diemthi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.diemthi.DataPropertyName = "DiemThi";
+            this.diemthi.HeaderText = "Điểm Thi";
+            this.diemthi.MinimumWidth = 6;
+            this.diemthi.Name = "diemthi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 465);
+            this.Controls.Add(this.dataGridViewDiem);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_find);
+            this.Controls.Add(this.txt_gender);
+            this.Controls.Add(this.txt_faculty);
+            this.Controls.Add(this.dtp_dob);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tìm Kiếm Thông Tin Sinh Viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDiem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.DateTimePicker dtp_dob;
+        private System.Windows.Forms.TextBox txt_faculty;
+        private System.Windows.Forms.TextBox txt_gender;
+        private System.Windows.Forms.Button btn_find;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.DataGridView dataGridViewDiem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaMonHoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenMonHoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diemthi;
+    }
+}
+
